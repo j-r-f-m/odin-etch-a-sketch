375005f1d17eea8c2cf5619ee51a904953ee8166
jrfm <moesch.jonas@protonmail.com>
2022-04-18 17:27:24 +0200

    basic grid

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-function createGrid (n=16) {
+function createGrid (n) {
     // create grid
     
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 1; i <= n; i++) {
+        // create div element that contains all elements of a row
         console.log(i);
+        const container = document.querySelector('.row-container');
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add(`column${i}`, 'row-element-container');
+        container.appendChild(rowContainer);
 
-        const container = document.querySelector('.container');
-        console.log(container)
-        const div = document.createElement('div');
-        div.classList.add('grid-tile');
-        div.textContent = `tile ${i}`;
+         
+        for (let j = 1; j <= n; j++) {
+            const containerRow = document.querySelector(`.column${i}`);
+            console.log(containerRow)
+            const rowElement = document.createElement('div');
+            rowElement.classList.add('grid-tile');
+            //rowElement.textContent = `${i}${j}`;
         
-        container.appendChild(div)
-
+            containerRow.appendChild(rowElement);
+        }
     }
 }
 
-//createGrid()
+createGrid(5)
 
 
-createGrid(16)
 
+/***
+ * i = 0;
+j = 0;
+const grid = document.querySelector('.container');
+const rowContainer = document.createElement('div');
+rowContainer.classList.add(`row${i}`);
+grid.appendChild(rowContainer);
+
+
+const containerRow = document.querySelector(`.row${i}`);
+const rowElement = document.createElement('div');
+rowElement.classList.add('grid-title');
+rowElement.textContent = `tile${i}${j}`;
+containerRow.appendChild(rowElement)
+console.log(containerRow)
+
+ */
