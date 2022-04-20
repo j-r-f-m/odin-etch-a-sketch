eb2f94665d9375aa038f566aaa130caf278e101a
jrfm <moesch.jonas@protonmail.com>
2022-04-20 22:11:32 +0200

    add basic hover functionality

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
-n = 32;
-n_grid = n * n;
-
-
-grid_size = 760;  //size is 760 x 760, needs to be the same size as in style.css
-
-ratio = grid_size / n; // size of one side of a tile
-console.log(ratio);
-
-for (let i = 1; i <= n_grid; i++ ) {
-    const selectContainer = document.querySelector('.container');
-    containerElement = document.createElement('div');
-    containerElement.classList.add('container-element');
-    
-    containerElement.style.width = `${ratio}px`;
-    containerElement.style.height = `${ratio}px`;
-    //containerElement.textContent = `${i}`;
-    selectContainer.appendChild(containerElement);
-}
-
-
-
-
-
-/**
- * function createGrid (n) {
-    // create grid
-    
-
-    for (let i = 1; i <= n; i++) {
-        // create div element that contains all elements of a row
-        console.log(i);
-        const container = document.querySelector('.row-container');
-        const rowContainer = document.createElement('div');
-        rowContainer.classList.add(`column${i}`, 'row-element-container');
-        container.appendChild(rowContainer);
-
-         
-        for (let j = 1; j <= n; j++) {
-            const containerRow = document.querySelector(`.column${i}`);
-            console.log(containerRow)
-            const rowElement = document.createElement('div');
-            rowElement.classList.add('grid-tile');
-            //rowElement.textContent = `${i}${j}`;
+function createGrid(n) {
+    n_grid = n * n;
+    grid_size = 450;  //size is 760 x 760, needs to be the same size as in style.css
+    ratio = grid_size / n; // size of one side of a tile
+    for (let i = 1; i <= n_grid; i++ ) {
+        const selectContainer = document.querySelector('.container');
+        containerElement = document.createElement('div');
+        containerElement.classList.add('container-element');
         
-            containerRow.appendChild(rowElement);
-        }
+        containerElement.style.width = `${ratio}px`;
+        containerElement.style.height = `${ratio}px`;
+        //containerElement.textContent = `${i}`;
+        selectContainer.appendChild(containerElement);
     }
 }
 
-createGrid(5)
- * 
- */
+
+function resetGrid() {
+    let allTiles = document.querySelectorAll('.contianer-element');
+    console.log(allTiles);
+    arrTiles = Array.from(allTiles);
+    for (let i = 0; i < arrTiles.length; i++) {
+        console.log(arrTiles[i])
+        
+    }
+}
+
+createGrid(16);
+
+// draw on specified area
+let tile = document.querySelector('.container');
+//console.log('tile: ' + tile);
+tile.addEventListener("mouseover", e => {
+    //console.log(e.target);
+    e.target.style.backgroundColor= '#000000'
+})
+
+// reset canvas
+const btn = document.querySelector('.btn');
+console.log(btn);
+btn.addEventListener("click", () => {
+   let n = prompt("How many squares per side?");
+   console.log(n);
+   resetGrid();
+})
+
+
+// test call functions
